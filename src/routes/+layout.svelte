147c4be91e73a5a2a06db728ddf6e274694b04e8
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-02-05 20:10:24 +0100

    Revert "Add hacky version of pomodoro s"
    This reverts commit 5278a825ee227365a2d58d62160dfdc124990a9b.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,13 @@
 <script>
   import "../app.css";
-  import SettingsModal from "$lib/SettingsModal.svelte";
-
-  let isOpen= false;
-
 </script>
 
 <main>
-  <button on:click={() => isOpen = true}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><circle cx="128" cy="128" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></circle><path d="M183.7,65.1q3.8,3.5,7.2,7.2l27.3,3.9a103.2,103.2,0,0,1,10.2,24.6l-16.6,22.1s.3,6.8,0,10.2l16.6,22.1a102.2,102.2,0,0,1-10.2,24.6l-27.3,3.9s-4.7,4.9-7.2,7.2l-3.9,27.3a103.2,103.2,0,0,1-24.6,10.2l-22.1-16.6a57.9,57.9,0,0,1-10.2,0l-22.1,16.6a102.2,102.2,0,0,1-24.6-10.2l-3.9-27.3q-3.7-3.5-7.2-7.2l-27.3-3.9a103.2,103.2,0,0,1-10.2-24.6l16.6-22.1s-.3-6.8,0-10.2L27.6,100.8A102.2,102.2,0,0,1,37.8,76.2l27.3-3.9q3.5-3.7,7.2-7.2l3.9-27.3a103.2,103.2,0,0,1,24.6-10.2l22.1,16.6a57.9,57.9,0,0,1,10.2,0l22.1-16.6a102.2,102.2,0,0,1,24.6,10.2Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
-  </button>
   <slot />
   <a href="https://github.com/brandstetterm/pomodoro">
     <svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><path d="M84,240a23.9,23.9,0,0,0,24-24V168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><path d="M172,240a23.9,23.9,0,0,1-24-24V168" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><path d="M152,168h16a23.9,23.9,0,0,1,24,24v8a23.9,23.9,0,0,0,24,24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><path d="M104,168H88a23.9,23.9,0,0,0-24,24v8a23.9,23.9,0,0,1-24,24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path><path d="M111.8,64A52,52,0,0,0,68,40a52,52,0,0,0-3.5,44.7A49.3,49.3,0,0,0,56,112v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.3,49.3,0,0,0-8.5-27.3A52,52,0,0,0,188,40a52,52,0,0,0-43.8,24Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></path></svg>
   </a>
 </main>
-
-{#if isOpen}
-  <SettingsModal onClose={() => isOpen = false} />
-{/if}
 
 <style>
   main {
@@ -33,23 +22,6 @@
     background-color: #242c3d;
   }
 
-  button {
-    color: #ff0057;
-    height: 32px;
-    width: 32px;
-    padding: 0;
-    margin: 0;
-    border: none;
-    background-color: transparent;
-    border-radius: 999px;
-    cursor: pointer;
-    position: absolute;
-    top: 16px;
-    right: 16px;
-  }
-  button:hover {
-    transform: scale(1.05);
-  }
 
   svg {
     height: 100%;
