5278a825ee227365a2d58d62160dfdc124990a9b
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-02-05 19:52:08 +0100

    Add hacky version of pomodoro s ettings

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,28 +1,48 @@
 import {writable} from "svelte/store";
+import {browser} from "$app/environment";
+
+let defaultSettings: PomodoroSettings | undefined;
+if (browser) {
+  defaultSettings = JSON.parse(localStorage.getItem("settings")!);
+}
+if (!defaultSettings) {
+  defaultSettings = {
+    pomodoroDuration: 1500,
+    shortBreakDuration: 300,
+    longBreakDuration: 900,
+  };
+}
 
 type PomodoroStatus = "IDLE" | "COMPLETED" | "PAUSED" | "RUNNING";
 type PomodoroMode = "POMODORO" | "SHORT_BREAK" | "LONG_BREAK";
+type PomodoroSettings = {
+  pomodoroDuration: number;
+  shortBreakDuration: number;
+  longBreakDuration: number;
+};
 
 export type PomodoroStore = {
   status: PomodoroStatus;
   mode: PomodoroMode;
   interval: NodeJS.Timer | undefined;
   time: number;
+  settings: PomodoroSettings;
 };
 
 const {subscribe, update} = writable<PomodoroStore>({
   status: "IDLE",
   mode: "POMODORO",
   interval: undefined,
-  time: 1500,
+  time: defaultSettings.pomodoroDuration,
+  settings: defaultSettings,
 });
 
 const resetPomodoroTime = () => {
   update((pomodoro) => {
     switch (pomodoro.mode) {
-      case "POMODORO": pomodoro.time = 1500; break;
-      case "SHORT_BREAK": pomodoro.time = 300; break;
-      case "LONG_BREAK": pomodoro.time = 900; break;
+      case "POMODORO": pomodoro.time = pomodoro.settings.pomodoroDuration; break;
+      case "SHORT_BREAK": pomodoro.time = pomodoro.settings.shortBreakDuration; break;
+      case "LONG_BREAK": pomodoro.time = pomodoro.settings.longBreakDuration; break;
     }
     return pomodoro;
   });
@@ -88,6 +108,16 @@ const changeMode = (mode: PomodoroMode) => {
   resetPomodoroTime();
 };
 
+const updateSettings = (settings: Partial<PomodoroSettings>) => {
+  update((pomodoro) => {
+    pomodoro.settings = {...pomodoro.settings, ...settings};
+    localStorage.setItem("settings", JSON.stringify(pomodoro.settings));
+    if (pomodoro.status === "IDLE") {
+      resetPomodoroTime();
+    }
+    return pomodoro;
+  });
+};
 
-export {subscribe, start, pause, changeMode};
+export {subscribe, start, pause, changeMode, updateSettings};
 
